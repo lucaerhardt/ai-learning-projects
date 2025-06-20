a7ad27a316f3db9775014a0f8f3b67ede0a14b7a
lucaerhardt <erhardtlj@gmail.com>
2025-06-20 14:40:00 -0700

    Create score_feedback.py

---
+++ day5_conditions_loops_input/score_feedback.py
@@ -0,0 +1,10 @@
+score = int(input("Enter your score: "))
+
+if score >= 90:
+    print("Grade: A – Excellent")
+elif score >= 80:
+    print("Grade: B – Good job")
+elif score >= 70:
+    print("Grade: C – Keep working")
+else:
+    print("Grade: D – Needs improvement")
