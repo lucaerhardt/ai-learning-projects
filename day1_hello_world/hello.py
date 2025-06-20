c3edac36dd5550e33255921764489f20ddcd3936
lucaerhardt <erhardtlj@gmail.com>
2025-06-20 13:39:39 -0700

    Create hello.py

---
+++ day1_hello_world/hello.py
@@ -0,0 +1,7 @@
+print("Hello, AI world!")
+
+name = "Gerd"
+hours_studied = 1
+goal = "Master AI in 5 months"
+
+print(f"Hi {name}, you studied {hours_studied} hour today. Let's crush your goal: {goal}!")
