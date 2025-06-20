bc9ef4e997916b67572cf8d1891de4987cf356d8
lucaerhardt <erhardtlj@gmail.com>
2025-06-20 14:35:29 -0700

    Create learner_card.py

---
+++ day4_ai_learner_card/learner_card.py
@@ -0,0 +1,9 @@
+name = "Gerd"
+current_skill_level = "Beginner"
+goal = "Master AI in 5 months"
+favorite_AI_topic = "ChatGPT and neural networks"
+
+print(f"Name: {name}")
+print(f"Current Skill Level: {current_skill_level}")
+print(f"Learning Goal: {goal}")
+print(f"Excited About: {favorite_AI_topic}")
